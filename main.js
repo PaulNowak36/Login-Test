f71fd8e4177fc52ae33f832ffc2e0a4214858fa7
PaulNowak36 <paul.nowak@algosup.com>
2022-11-21 16:09:51 +0100

    attempted mac windows switch

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const login = document.querySelector("#button");
-
-console.log(login);
-
-console.log(username);
-
-console.log(password);
 
 
-login.addEventListener("click", validate);
-function validate() {
+const macLogin = document.querySelector("#link");
+const windowLogin = document.querySelector("#button");
+
+if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+    alert("Mac"); // You can do whatever here
+    windowLogin.style.display = "none";
+    macLogin.addEventListener("click", macValidate);
+    console.log(macLogin);
+
+} else {
+    alert("Windows"); // You can do whatever here
+    macLogin.style.display = "none";
+    windowLogin.addEventListener("click", windowsValidate);
+    console.log(windowLogin);
+}
+
+
+//macLogin.addEventListener("click", validate);
+function macValidate() {
 
 
     console.log('hello');
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
     
     if (username.value == "admin" && password.value == "admin") {
-        login.href='Hello.html';
+        macLogin.href='Hello.html';
         alert("Login successfully");
-
-        //window.location("Hello.html");
-	    //alert("Login successfully");
     }
     else if (username.value == "" && password.value == "") {
-        login.href='';
+        alert("Please type something");
+        macLogin.href='';
     }  
     else {
         alert("Invalid username or password");
         return false;
     } 
+}
+
+function windowsValidate() {
+    var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if (username == "admin" && password == "admin") {		
+		window.location.assign("Hello.html");
+	   	alert("Login successfully");
+	} else {
+	alert("Invalid username or password");
+	   return false;
+	}
 }
